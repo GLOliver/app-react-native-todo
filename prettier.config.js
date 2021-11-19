fab4e79cc0124702fb2be610d51ca30362376f41
GLOliver <gui.lopes11@gmail.com>
2021-11-18 23:14:13 -0300

    Implementação dos primeiros componentes - 34:48

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const opitions = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    backetSpacing: true,
+    endOfLie: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+module.exports = opitions
